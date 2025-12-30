e2353c6c28aae3030bd12ab926f4c83ce0a23741
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-12-30 23:55:31 +0530

    feat: Implemented Apollo Client setup and URL resolver with JWT authentication for protected routes

---
+++ client/src/lib/apollo.js
@@ -0,0 +1,24 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { SetContextLink } from "@apollo/client/link/context";
+
+// GraphQL endpoint
+const httpLink = new HttpLink({
+    uri: "http://localhost:3000/api/graphql",
+});
+
+const authLink = SetContextLink((_, {headers}) => {
+    const token = localStorage.getItem('token');
+
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+// Instance of Apollo Client 
+export const client = new ApolloClient({
+    link: authLink.concat(httpLink), // Auth ko main link se chain kar diya
+    cache: new InMemoryCache(), // Cache ko initialize kar diya
+});
--- server/src/url/url.resolver.ts
+++ server/src/url/url.resolver.ts
@@ -1,4 +1,47 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Int, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { UrlService } from  './url.service.js';
+import { Url } from './entities/url.entity.js';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard.js';
+import { CreateUrlInput } from './dto/create-url.input.js';
+import { UseGuards } from '@nestjs/common';
 
-@Resolver()
-export class UrlResolver {}
+@Resolver(() => Url)
+export class UrlResolver {
+    constructor(private urlService: UrlService) {}
+
+    // Protected Route - To create the Chotu URL
+    @Mutation(() => Url)
+    @UseGuards(JwtAuthGuard)
+    async createUrl(
+        @Args('input') input: CreateUrlInput,
+        @Context() context: any
+    ) {
+        const userId = context.req.user.id;
+        return this.urlService.createUrl(userId, input);
+    }
+
+    // Protected Route - To get the all Urls
+    @Query(() => [Url])
+    @UseGuards(JwtAuthGuard)
+    async myUrls(@Context() context: any) {
+        const userId = context.req.user.id;
+        return this.urlService.getUserUrls(userId);
+    }
+
+    // Protected Route - To Delte a Chotu Url
+    @Mutation(() => Boolean)
+    @UseGuards(JwtAuthGuard)
+    async deleteUrl(
+        @Args('urlId') urlId: string,
+        @Context() context: any
+    ) {
+        const userId = context.req.user.id;
+        return this.urlService.deleteUrl(userId, urlId);
+    }
+
+    // To check Chotu Url clicks
+    @ResolveField(() => Int)
+    clickCount(@Parent() url: any) {
+        return url._count?.clicks || 0;
+    }
+}
